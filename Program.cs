88a7685af5b7f81c03a11501baa128423d24bd92
Mr Mixailov <Mr_Mixailov@yandex.ru>
2021-12-27 20:49:10 +0300

    подправил решение

--- Program.cs
+++ Program.cs
@@ -2,12 +2,24 @@
 чисел формирует массив из чисел, больших 8. Первоначальный 
 массив можно ввести с клавиатуры, либо сгенерировать случайным образом. 
 При решении не рекомендуется пользоваться коллекциями, 
-лучше обойтись исключительно массивами./*/
-int[] array = new int[8];
-        for (int i = 0; i < array.Length; i++){
-            array[i] = (new Random().Next(9,20));
-            Console.Write(array[i] + " ");}
-            //картинка с блок схемой прилагается 111.jpg
+лучше обойтись исключительно массивами.
+/*/
+int[] array = new int[10];
+Console.Write("Массив сгенерированный случайным образом - [ ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = (new Random().Next(1,20));
+            Console.Write(array[i]+", ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+            Console.Write("Сформированный массив из чисел, больших 8 - [ ");
+    for (int i = 0; i < array.Length; i++){
+        if (array[i] > 8){
+            Console.Write($"{array[i]}, " + string.Empty);}    
+    }
+    Console.Write("]"); 
+            //картинка с блок схемой прилагается 222.jpg
                 
                         
           
